f3252ebb269b8d879ec5cd89c7bff35905b3141e
CapnHazard <modiyash301@gmail.com>
2026-07-08 21:47:45 +0530

    Implemented type command [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,22 @@ public class Main {
             String command = sc.nextLine();
             if(command.equals("exit")) {
                 break;
-            } else if(command != "exit" && !command.startsWith("echo")) {
+            } else if(command != "exit" && !command.startsWith("echo") && !command.startsWith("type")) {
                 System.out.print(command+": command not found\n");
             }
             if(command.startsWith("echo")) {
                 System.out.println(command.substring(4, command.length()).strip());
-                
+            }
+            if(command.startsWith("type")) {
+                if(command.startsWith("echo", 5)) {
+                    System.out.println("echo is a shell built-in");
+                } else if(command.startsWith("exit", 5)) {
+                    System.out.println("exit is a shell built-in");
+                } else if(command.startsWith("type")) {
+                    System.out.println("type is a shell built-in");
+                } else {
+                    System.out.println("<" + command.substring(5) + ">: not found");
+                }
             }
         }
         sc.close();
