91b469ca3773bf83647ed7370d0a7a799062f928
CapnHazard <modiyash301@gmail.com>
2026-07-14 11:34:57 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -51,7 +51,7 @@ public class Main {
                     if(new File(rest).isAbsolute()) {
                         dir = new File(rest);
                     } else if(rest.equals("~")) {
-                        System.getProperty("user.home");
+                        currentDirectory = System.getProperty("user.home");
                     } else {
                         dir = new File(currentDirectory, rest);
                     }
